efa5747b3f72154bb62be36bfaa5015e3c52d0d9
Manal Albahar <manal.albahar@gmail.com>
2022-03-07 14:24:27 +0300

    ll test solved

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -17,19 +17,17 @@ class LinkedList {
         }
     }
 
-    append(value) {
-        const node = new Node(value);
-        if(!this.head) { 
-            this.head = node;
-        } else {
-            let currentNode = this.head;
-
-            while (currentNode.next) {
-                currentNode = currentNode.next;
-            }
-            currentNode.next = node;
-        }
-    }
+    includes(value){
+        if(this.head!=null){
+        let headV = this.head;
+        while (headV!=null) {
+          if(headV.value!=value)
+          headV = headV.next;
+          else return true;}
+          return false;
+      }
+      else return false;
+      }
 includes(value){
     if(this.head!=null){
         let headV =this.head;
@@ -45,16 +43,18 @@ else return false;
 toString(){
     let values="";
     if(this.head!=null){
-        let headV=this.head;
-        while(headV!=null){
-            values=values+`{${headV.value}->}`;
-            headV=headV.next;
-        }
-        values=values=`NULL`;
-        return values;
-    }
-    else return "list is empty";
-}
+    let headV = this.head;
+    while (headV!=null) {
+      values  =values+`{ ${headV.value} } -> `;
+      headV = headV.next;
+  }
+  values  =values +`NULL`;
+  return values;
+  }
+  else return "empty list";
+  
+  }
+  
 }
 
 
